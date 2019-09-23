da3d8e905ed91263112e94f463857c18d0783746
flamerged <mersad.ajanovic@googlemail.com>
2019-09-24 00:43:38 +0200

    added bodyparser and cookie parser modules

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get('/', (request, response) => {
     response.render('index');
@@ -12,6 +16,15 @@ app.get('/cards', (request, response) => {
         prompt: 'Who is buried in my Garden!',
         hint: 'I am your Fater'
     });
+});
+
+app.get('/hello', (req, res) => {
+    res.render('hello', { name: req.cookies.username });
+});
+
+app.post('/hello', (req, res) => {
+    res.cookie('username', req.body.username);
+    res.render('hello', { name: req.body.username });
 });
 
 app.listen(1337, () => {
